8d987f5b78914b350cd77d33096ef1accb6bf47f
Fernanda <fernanda@platan.us>
2018-09-07 10:01:47 -0300

    chore(): fix redis reference

--- lib/discourse_redis.rb
+++ lib/discourse_redis.rb
@@ -122,8 +122,9 @@ class DiscourseRedis
   end
 
   def self.raw_connection(config = nil)
-    config ||= self.config
-    Redis.new(config)
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    Resque.redis = REDIS
   end
 
   def self.config
